dda7fc280907cdd791d26f94c1303aab9432e920
ricer0ll <brianle17360@gmail.com>
2026-03-13 01:34:33 -0700

    added toast notifications for email forms

--- js/index.js
+++ js/index.js
@@ -16,105 +16,134 @@ function setExperienceSection(jsonFile) {
   fetch(jsonFile)
     .then((response) => response.json())
     .then((data) => {
-        const listOfExperience = data.experience;
+      const listOfExperience = data.experience;
 
-        listOfExperience.forEach((experience) => {
-            addExperience(experience.image, experience.role, experience.date, experience.description, experience.alt)
-        })
+      listOfExperience.forEach((experience) => {
+        addExperience(
+          experience.image,
+          experience.role,
+          experience.date,
+          experience.description,
+          experience.alt,
+          experience.link,
+        );
+      });
     });
-
 }
 
 function setProjectsSection(jsonFile) {
   fetch(jsonFile)
     .then((response) => response.json())
     .then((data) => {
-        const listOfProjects = data.project;
+      const listOfProjects = data.project;
 
-        listOfProjects.forEach((project) => {
-            addProject(project.image, project.name, project.date, project.description, project.alt)
-        })
+      listOfProjects.forEach((project) => {
+        addProject(
+          project.image,
+          project.name,
+          project.date,
+          project.description,
+          project.alt,
+          project.link,
+        );
+      });
     });
-
 }
 
-function addExperience(image, role, date, description, alt) {
+function addExperience(image, role, date, description, alt, link) {
   $("#experience")
-  .append(
-    $(`<div class="col-md-12 col-lg-4 d-flex justify-content-center my-5">`)
-      .append(`<img id="experienceBanner" src="assets/${image}" alt=${alt} class="rounded img-fluid"/>`)
-  )
-  .append(
-    $(`<div class="col-lg-3">`)
-  )
-  .append(
-    $(`<div class="col-md-12 col-lg-5 text-lg-start">`)
+    .append(
+      $(
+        `<div class="col-md-12 col-lg-4 d-flex justify-content-center my-5">`,
+      ).append(
+        $(`<a href=${link} target="_blank">`).append(
+          $(
+            `<img id="experienceBanner" src="assets/${image}" alt=${alt} class="rounded img-fluid"/>`,
+          ),
+        ),
+      ),
+    )
+    .append($(`<div class="col-lg-3">`))
+    .append(
+      $(`<div class="col-md-12 col-lg-5 text-lg-start">`)
         .append($(`<h1>`).text(role))
         .append($(`<h5>`).text(date))
-        .append($(`<p class="mt-3">`).text(description))
-  );
+        .append($(`<p class="mt-3">`).text(description)),
+    );
 }
 
-function addProject(image, name, date, description, alt) {
+function addProject(image, name, date, description, alt, link) {
   $("#projects")
-  .append(
-    $(`<div class="col-md-12 col-lg-5 d-flex justify-content-center my-5">`)
-      .append(`<img id="projectBanner" src="assets/${image}" alt=${alt} class="rounded img-fluid"/>`)
-  )
-  .append(
-    $(`<div class="col-lg-2">`)
-  )
-  .append(
-    $(`<div class="col-md-12 col-lg-5 mt-5 text-lg-start">`)
+    .append(
+      $(
+        `<div class="col-md-12 col-lg-5 d-flex justify-content-center my-5">`,
+      ).append(
+        $(`<a href=${link} target="_blank">`).append(
+          $(
+            `<img id="projectBanner" src="assets/${image}" alt=${alt} class="rounded img-fluid"/>`,
+          ),
+        ),
+      ),
+    )
+    .append($(`<div class="col-lg-2">`))
+    .append(
+      $(`<div class="col-md-12 col-lg-5 mt-5 text-lg-start">`)
         .append($(`<h1>`).text(name))
         .append($(`<h5>`).text(date))
-        .append($(`<p>`).text(description))
-  );
+        .append($(`<p>`).text(description)),
+    );
 }
 
 function isValidEmail(email) {
-    // Credit: https://www.geeksforgeeks.org/javascript/javascript-program-to-validate-an-email-address/
+  // Credit: https://www.geeksforgeeks.org/javascript/javascript-program-to-validate-an-email-address/
 
-    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return pattern.test(email);
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(email);
 }
 
 // EmailJS docs: https://www.emailjs.com/docs/tutorial/overview/
 emailjs.init({
-  publicKey: 'YTMD_35H_10BWvLt4',
+  publicKey: "YTMD_35H_10BWvLt4",
   blockHeadless: true,
   blockList: {
-    list: []
+    list: [],
   },
   limitRate: {
     // Set the limit rate for the application
-    id: 'app',
+    id: "app",
     // Allow 1 request per 10s
     throttle: 10000,
   },
 });
+
 
 const jsonFile = "js/section_text.json";
 setAboutSection(jsonFile);
 setExperienceSection(jsonFile);
 setProjectsSection(jsonFile);
 
-const emailSubmit = document.querySelector("#contactForm")
-emailSubmit.addEventListener("submit", event => {
-    event.preventDefault();
+const emailSubmit = document.querySelector("#contactForm");
+emailSubmit.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    const templateParams = {
-        "name": document.querySelector("#inputName").value,
-        "message": document.querySelector("#inputMsg").value,
-        "email": document.querySelector("#inputEmail").value
-    };
-    
-    // Make sure the email is valid
-    if (!isValidEmail(templateParams.email)) {
-        console.log(templateParams.email);
-        return;
-    }
+  const toastLiveExample = document.getElementById('liveToast')
 
-    emailjs.send("service_roewke7", "template_ujdrkdg", templateParams)
-    console.log("Sent email")
-})
+  const templateParams = {
+    name: document.querySelector("#inputName").value,
+    message: document.querySelector("#inputMsg").value,
+    email: document.querySelector("#inputEmail").value,
+  };
+
+  // Make sure the email is valid
+  if (!isValidEmail(templateParams.email)) {
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+    myModal.show();
+    return;
+  }
+
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastBootstrap.show()
+
+  //emailjs.send("service_roewke7", "template_ujdrkdg", templateParams);
+  console.log("Sent email");
+});
